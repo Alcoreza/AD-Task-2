b5131ae7cbb5f5b2eb2241c99b3a7745b5a0afa0
Alcoreza <202310642@fit.edu.ph>
2025-05-16 17:56:05 +0800

    fix(landingPage) - fixed the linking of components

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-include './components/header.component.php';
-include './utils/car.utils.php';
-include './handlers/cars.handler.php';
+include 'components/header.component.php';
+include 'utils/car.utils.php';
+include 'handlers/car.handler.php';
 ?>
 
 <div class="banner">Welcome to AD-CarSite - Explore Our Cars</div>
 <div class="car-list">
     <?php
     foreach ($cars as $car) {
-        include './components/templates/car-card.component.php';
+        include 'components/templates/car-card.component.php';
     }
     ?>
 </div>
 
-<?php include './components/footer.component.php'; ?>
+<?php include 'components/footer.component.php'; ?>

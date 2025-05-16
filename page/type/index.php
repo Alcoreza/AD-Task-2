f2c0b85ab7c0fe59695fdba60c8266519f886d04
Alcoreza <202310642@fit.edu.ph>
2025-05-16 18:18:28 +0800

    feat(types) - added the html of the types page

---
+++ page/type/index.php
@@ -0,0 +1,23 @@
+<?php
+include '../../components/header.component.php';
+include '../../utils/car.utils.php';
+$cars = getCars();
+
+$types = [];
+foreach ($cars as $car) {
+    $types[$car['type']][] = $car;
+}
+?>
+
+<link rel="stylesheet" href="./assets/css/types.css" />
+
+<div class="banner">Welcome to Car Types</div>
+<?php foreach ($types as $type => $typeCars): ?>
+    <h2><?php echo $type; ?></h2>
+    <div class="car-list">
+        <?php foreach ($typeCars as $car)
+            include '../../components/templates/car-card.component.php'; ?>
+    </div>
+<?php endforeach; ?>
+
+<?php include '../../components/footer.component.php'; ?>

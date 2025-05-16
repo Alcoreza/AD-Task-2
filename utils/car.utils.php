dae385b4bf1be5724dd9b484bb9ff507bcebc04c
Alcoreza <202310642@fit.edu.ph>
2025-05-16 17:25:16 +0800

    feat - added a dictionary in utils

---
+++ utils/car.utils.php
@@ -0,0 +1,67 @@
+<?php
+function getCars()
+{
+    return [
+        [
+            'name' => 'Model X',
+            'brand' => 'Tesla',
+            'type' => 'Electric',
+            'image' => 'car1.jpg',
+        ],
+        [
+            'name' => 'Mustang GT',
+            'brand' => 'Ford',
+            'type' => 'Sports',
+            'image' => 'car2.jpg'
+        ],
+        [
+            'name' => 'Civic',
+            'brand' => 'Honda',
+            'type' => 'Sedan',
+            'image' => 'car3.jpg'
+        ],
+        [
+            'name' => 'Corolla',
+            'brand' => 'Toyota',
+            'type' => 'Sedan',
+            'image' => 'car4.jpg'
+        ],
+        [
+            'name' => 'Wrangler',
+            'brand' => 'Jeep',
+            'type' => 'SUV',
+            'image' => 'car5.jpg'
+        ],
+        [
+            'name' => 'i8',
+            'brand' => 'BMW',
+            'type' => 'Hybrid',
+            'image' => 'car6.jpg'
+        ],
+        [
+            'name' => 'A4',
+            'brand' => 'Audi',
+            'type' => 'Sedan',
+            'image' => 'car7.jpg'
+        ],
+        [
+            'name' => 'Cybertruck',
+            'brand' => 'Tesla',
+            'type' => 'Electric',
+            'image' => 'car8.jpg'
+        ],
+        [
+            'name' => 'F-150 Raptor',
+            'brand' => 'Ford',
+            'type' => 'Truck',
+            'image' => 'car9.jpg'
+        ],
+        [
+            'name' => 'Camaro',
+            'brand' => 'Chevrolet',
+            'type' => 'Sports',
+            'image' => 'car10.jpg'
+        ],
+    ];
+}
+?>

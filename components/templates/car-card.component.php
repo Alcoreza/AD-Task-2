a418252f93e7949f4b6c1170cce0d70254e50595
Alcoreza <202310642@fit.edu.ph>
2025-05-16 17:11:15 +0800

    feat - added a card template

---
+++ components/templates/car-card.component.php
@@ -0,0 +1,6 @@
+<div class="car-card">
+    <img src="/assets/img/<?php echo $car['image']; ?>" alt="<?php echo $car['name']; ?>">
+    <h3><?php echo $car['name']; ?></h3>
+    <p>Brand: <?php echo $car['brand']; ?></p>
+    <p>Type: <?php echo $car['type']; ?></p>
+</div>

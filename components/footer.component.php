f41cf39cffb9d234473e85be88c4551720ecb603
Alcoreza <202310642@fit.edu.ph>
2025-05-16 17:06:37 +0800

    feat - created the header and footer component

---
+++ components/footer.component.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; 2025 AD-CarSite</p>
+</footer>
+</body>
+
+</html>
--- components/header.component.php
+++ components/header.component.php
@@ -1,8 +1,21 @@
-<header>
-    <ig src="" alt="CarHub Logo" />
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="">Brands</a>
-        <a href="">Types</a>
-    </nav>
-</header>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CarHub</title>
+</head>
+
+<body>
+    <header>
+        <h1>CarHub</h1>
+        <nav>
+            <a href="">Home</a>
+            <a href="">Brands</a>
+            <a href="">Types</a>
+        </nav>
+    </header>
+</body>
+
+</html>

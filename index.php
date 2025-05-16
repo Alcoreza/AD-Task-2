ef08424f25947a69c92623f6895329fa6bcfd03b
Alcoreza <202310642@fit.edu.ph>
2025-05-16 16:44:34 +0800

    feat(landingPage) - added the landing page structure

--- index.php
+++ index.php
@@ -9,10 +9,31 @@
 
 <body>
     <?php
-    echo "Hello";
-    echo "Elmer Daniel Alcoreza";
-    echo "Hello";
-    echo "Hello";
+    include "components/header.component.php";
+    ?>
+
+    <div class="welcome-banner">
+        <h1>Welcome to CarHub</h1>
+        <p>Your Gateway to the world of automobile</p>
+    </div>
+
+    <div class="card">
+        <h2>Featured Brands</h2>
+        <div class="brand-list">
+            <?php
+            $brands = ['Audi', 'BMW', 'Toyota', 'Mercedes-Benz', ' Honda', 'Ford'];
+            foreach ($brands as $brand) {
+                echo "<div class='brand-item'>
+                <img src='assets/img/" . strtolower(str_replace(' ', '', $brand)) . ".jpg' alt='$brand'/>
+                <p>$brand</p>
+                </div>";
+            }
+            ?>
+        </div>
+    </div>
+
+    <?php
+    include 'components/footer.component.php';
     ?>
 </body>
 

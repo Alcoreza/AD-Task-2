fe8531f78ddbf2282cfb7aab2f5eca0daa47d8e3
Alcoreza <202310642@fit.edu.ph>
2025-05-16 22:12:54 +0800

    feat(types) - redesigned the page with colors and card sizes

--- page/type/index.php
+++ page/type/index.php
@@ -9,14 +9,29 @@ foreach ($cars as $car) {
 }
 ?>
 
-<link rel="stylesheet" href="../assets/css/types.css" />
+<link rel="stylesheet" href="../../assets/css/global.css" />
+<link rel="stylesheet" href="assets/css/types.css" />
 
-<div class="banner">Welcome to Car Types</div>
+<div class="types-banner">
+    <div class="overlay"></div>
+    <div class="banner-text">
+        <h1>Explore Car Types</h1>
+        <p>Discover the diversity and characteristics of various car types</p>
+    </div>
+</div>
+
+<div class="info-section white">
+    Cars come in many types, from sleek sports cars to versatile SUVs. Understanding these types helps you choose the
+    right vehicle to suit your lifestyle and needs.
+</div>
+
 <?php foreach ($types as $type => $typeCars): ?>
-    <h2><?php echo $type; ?></h2>
-    <div class="car-list">
-        <?php foreach ($typeCars as $car)
-            include '../../components/templates/car-card.component.php'; ?>
+    <div class="type-block">
+        <h2 class="type-title"><?php echo $type; ?></h2>
+        <div class="type-car-grid">
+            <?php foreach ($typeCars as $car)
+                include '../../components/templates/car-card.component.php'; ?>
+        </div>
     </div>
 <?php endforeach; ?>
 
